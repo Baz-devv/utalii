8e8a30a528515d8649aa4d87f2571b0fc64ddc30
Baz-devv <123078644+Baz-devv@users.noreply.github.com>
2023-11-09 03:13:13 +0300

    Update viewsController.js

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -13,11 +13,8 @@ exports.alerts = (req, res, next) => {
 };
 
 exports.getOverview = catchAsync(async (req, res, next) => {
-  // 1) Get tour data from collection
   const tours = await Tour.find();
 
-  // 2) Build template
-  // 3) Render that template using tour data from 1)
   res.status(200).render('overview', {
     title: 'All Tours',
     tours
@@ -25,7 +22,6 @@ exports.getOverview = catchAsync(async (req, res, next) => {
 });
 
 exports.getTour = catchAsync(async (req, res, next) => {
-  // 1) Get the data, for the requested tour (including reviews and guides)
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: 'reviews',
     fields: 'review rating user'
@@ -35,8 +31,6 @@ exports.getTour = catchAsync(async (req, res, next) => {
     return next(new AppError('There is no tour with that name.', 404));
   }
 
-  // 2) Build template
-  // 3) Render template using data from 1)
   res.status(200).render('tour', {
     title: `${tour.name} Tour`,
     tour
@@ -56,10 +50,8 @@ exports.getAccount = (req, res) => {
 };
 
 exports.getMyTours = catchAsync(async (req, res, next) => {
-  // 1) Find all bookings
   const bookings = await Booking.find({ user: req.user.id });
 
-  // 2) Find tours with the returned IDs
   const tourIDs = bookings.map(el => el.tour);
   const tours = await Tour.find({ _id: { $in: tourIDs } });
 
