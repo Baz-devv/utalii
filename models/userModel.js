6b9f824b07496cf64f8d5349c9fe76fefa9a8510
Baz-devv <123078644+Baz-devv@users.noreply.github.com>
2023-11-09 03:06:06 +0300

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -34,7 +34,6 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please confirm your password'],
     validate: {
-      // This only works on CREATE and SAVE!!!
       validator: function(el) {
         return el === this.password;
       },
@@ -52,13 +51,10 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.pre('save', async function(next) {
-  // Only run this function if password was actually modified
   if (!this.isModified('password')) return next();
 
-  // Hash the password with cost of 12
   this.password = await bcrypt.hash(this.password, 12);
 
-  // Delete passwordConfirm field
   this.passwordConfirm = undefined;
   next();
 });
@@ -71,7 +67,6 @@ userSchema.pre('save', function(next) {
 });
 
 userSchema.pre(/^find/, function(next) {
-  // this points to the current query
   this.find({ active: { $ne: false } });
   next();
 });
@@ -93,7 +88,6 @@ userSchema.methods.changedPasswordAfter = function(JWTTimestamp) {
     return JWTTimestamp < changedTimestamp;
   }
 
-  // False means NOT changed
   return false;
 };
 
@@ -104,8 +98,6 @@ userSchema.methods.createPasswordResetToken = function() {
     .createHash('sha256')
     .update(resetToken)
     .digest('hex');
-
-  // console.log({ resetToken }, this.passwordResetToken);
 
   this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
 
